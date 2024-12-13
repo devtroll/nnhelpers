61e8e5289cc53548bc3be2622630f67360dee5c6
David Bascom <david@99grad.de>
2024-12-13 13:08:19 +0100

    [TASK] update query builder

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,8 +6,8 @@ CREATE TABLE tx_nnhelpers_domain_model_entry (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
-	title text DEFAULT '' NOT NULL,
-	data text DEFAULT '' NOT NULL,
+	title text,
+	data text,
 	media int(11) unsigned NOT NULL default '0',
 	categories int(11) unsigned NOT NULL default '0',
 	
