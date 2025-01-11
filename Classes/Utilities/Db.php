0be38de74250a8e35a5caac023c32e2fcd85a0e2
David Bascom <david@99grad.de>
2025-01-11 15:28:25 +0100

    [FEATURE] added insertMultiple for bulk insert

--- Classes/Utilities/Db.php
+++ Classes/Utilities/Db.php
@@ -597,6 +597,54 @@ class Db implements SingletonInterface
 	}
 	
 	/**
+	 * Mehrere Zeilen in Datenbank einfügen.
+	 * ```
+	 * use TYPO3\CMS\Core\Database\Connection;
+	 * 
+	 * $data = [
+	 * 	['title' => 'Eins', 'tstamp'=>123],
+	 * 	['title' => 'Zwei', 'tstamp'=>123],
+	 * ];
+	 * $colOrder = [
+	 * 	'tstamp' => Connection::PARAM_INT,
+	 * 	'title' => Connection::PARAM_STR,
+	 * ];
+	 * 
+	 * \nn\t3::Db()->insertMultiple('table', $data, $colOrder);
+	 * ```
+	 * @param string $tableName
+	 * @param array $rows
+	 * @param array $colOrder
+	 * @return boolean
+	 */
+	public function insertMultiple ( $tableName = '', $rows = [], $colOrder = [] ) 
+	{
+		$connection = $connection = $this->getConnection();
+
+		if (!$rows) {
+			return true;
+		}
+
+		$flattened = [];
+		foreach ($rows as $row) {
+			$insert = [];
+			foreach ($colOrder as $col=>$type) {
+				$insert[] = $row[$col] ?? '';
+			}
+			$flattened[] = $insert;
+		}
+
+		$result = $connection->bulkInsert( 
+			$tableName,
+			$flattened,
+			array_keys( $colOrder ),
+			array_values( $colOrder ),
+		);
+
+		return $result;
+	}
+
+	/**
 	 * Datenbank-Eintrag erstellen ODER einen vorhandenen Datensatz updaten.
 	 * 
 	 * Entscheidet selbstständig, ob der Eintrag per `UPDATE` oder `INSERT` in die Datenbank
